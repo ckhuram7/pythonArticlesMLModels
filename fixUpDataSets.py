16771cd6749f65473e6f232f96717793d728f8f3
Khuram Chughtai <ckhuram7@vt.edu>
2019-05-01 02:01:50 -0400

    Updating Class to create train and test data

--- fixUpDataSets.py
+++ fixUpDataSets.py
@@ -115,23 +115,67 @@ class DataPrep:
     def makeTestTrainFiles(self, fileNames, **kwargs):
         """
         This function will read in the raw data files and parse them accordingly to create test and train datasets
+
+        @param  fileNames   (list)  List of files with articles that need to be read in
+        @param  columnNames (list)  List of Column Names that should be kept from the file that is read in
+        @param  numOfFeatures   (int)   This is the number of the different features that should be returned
+        @param  featureCol  (str)   The column name that will be used as a feature that we will be testing for and training on
+        @param  numOfRecPerFile (int)   THe number of rows that each file created will have
         """
-        # Global Variables
+        # Local Function Variables
         listOfDataFrames = []
-        listOfColumns
+        listOfColumns = []
+        numOfFeatures = 5
+        featureCol = ''
+
+        # Reading in the arguments provided by the function
+		for key in kwargs:
+			# This will look at different Given Arguments Passed by a Class and process them accordingly
+			if str(key).lower() == 'given_argument':
+				pass
+			elif str(key).lower() == 'columnnames':
+				listOfColumns = kwargs[key]
+			elif str(key).lower() == 'numoffeatures':
+				numOfFeatures = kwargs[key]
+            elif str(key).lower() == 'featurecol':
+				featureCol = kwargs[key]
+
         self.log.info("Starting to Read file to create Test and Train Data Files formatting them as needed")
         self.log.info("Received the following fileNames for the raw files to create test and train data from: {}".format(fileNames))
         for fileName in fileNames:
             # For Each Data Record within this scope already has an index so it will be redundant to include another index and waste
             # of computer resources making another column
             listOfDataFrames.append(pandas.read_csv(filepath_or_buffer = fileName, index_col= false))
+        
+        # Now to Concat the list of DataFrames if there is more than one.
+        if len(fileNames) > 1:
+            self.log.info("Since more than one file was given, using pandas concat function to combine files into one DataFrame")
+            opsDF = pandas.concat(listOfDataFrames)
+        else:
+            #Will Go Into this test If the size of the DataFrame is 1 or less
+            opsDf = listOfDataFrames[0]
+        self.log.info("Generated Dataframe has the following dimensions: {}".format(str(opsDF.shape)))
+        self.log.info("Generated Dataframe columns: {}".format(str(opsDF.columns)))
+        
+        # Now that we have the given fileName we will reduce the numberOfColumns to the column list provided
+        #Checking to see if provided argument has a length greater than 0
+        if len(listOfColumns) > 0:
+            # Making sure that the list of Columns is not greater than what we expect to have from DataFrame
+            if len(listOfColumns) < len(opsDF.columns):
+                # Checking to see all of the required columns are present in the DataFrame
+                if set(listOfColumns).issubset(opsDF.columns):
+                    opsDF = opsDF[listOfColumns]
+                else:
+                    self.log.error("Failure when checking if listOfColumns exist in pandas DataFrame: {}".format(set(listOfColumns).issubset(opsDF.columns)))
+                    raise IndexError("Not all elements requested are part of pandas Dataframe")
+        else:
 
 
 
 
 if __name__ == '__main__':
 	workerClass = WordScraper(name = "Week5", logging=True)
-	myTagger = nltk.load('taggers/maxent_treebank_pos_tagger/english.pickle')
+	myTagger = nltk.load('taggers/maxent_treebank_pos_tagger/english.pickle'
 
 	# When Read in From File
 	text = workerClass.readFile(fileName = "input.txt")
